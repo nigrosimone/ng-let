f924948f3b1d7a158bd3342748c081177bf81d35
Nigro Simone <nigro.simone@gmail.com>
2021-05-31 20:45:11 +0200

    Angular 12

--- projects/ng-let-demo/src/app/app.module.ts
+++ projects/ng-let-demo/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
-import { NgLetModule } from 'ng-let';
+import { NgLetModule } from '../../../ng-let/src/public-api';
 
 @NgModule({
   declarations: [AppComponent],
