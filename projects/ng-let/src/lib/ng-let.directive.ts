a44b430dd1fd8b650b1e626dc75596fc2041b8d6
Nigro Simone <nigro.simone@gmail.com>
2021-12-18 09:55:46 +0100

    Add ngTemplateContextGuard

--- projects/ng-let/src/lib/ng-let.directive.ts
+++ projects/ng-let/src/lib/ng-let.directive.ts
@@ -10,12 +10,12 @@ interface NgLetContext<T> {
     selector: '[ngLet]'
 })
 export class NgLetDirective<T> {
-    
+
     private context: NgLetContext<T | null> = { ngLet: null, $implicit: null };
     private hasView: boolean = false;
 
     // eslint-disable-next-line no-unused-vars
-    constructor(private viewContainer: ViewContainerRef, private templateRef: TemplateRef<NgLetContext<T>>) {}
+    constructor(private viewContainer: ViewContainerRef, private templateRef: TemplateRef<NgLetContext<T>>) { }
 
     @Input()
     set ngLet(value: T) {
@@ -24,5 +24,28 @@ export class NgLetDirective<T> {
             this.viewContainer.createEmbeddedView(this.templateRef, this.context);
             this.hasView = true;
         }
+    }
+
+    /** @internal */
+    public static ngLetUseIfTypeGuard: void;
+
+    /**
+     * Assert the correct type of the expression bound to the `NgLet` input within the template.
+     *
+     * The presence of this static field is a signal to the Ivy template type check compiler that
+     * when the `NgLet` structural directive renders its template, the type of the expression bound
+     * to `NgLet` should be narrowed in some way. For `NgLet`, the binding expression itself is used to
+     * narrow its type, which allows the strictNullChecks feature of TypeScript to work with `NgLet`.
+     */
+    static ngTemplateGuard_ngLet: 'binding';
+
+    /**
+     * Asserts the correct type of the context for the template that `NgLet` will render.
+     *
+     * The presence of this method is a signal to the Ivy template type-check compiler that the
+     * `NgLet` structural directive renders its template with a specific context type.
+     */
+    static ngTemplateContextGuard<T>(dir: NgLetDirective<T>, ctx: any): ctx is NgLetContext<Exclude<T, false | 0 | '' | null | undefined>> {
+        return true;
     }
 }
