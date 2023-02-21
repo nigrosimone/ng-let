3e29d679544fbc78cad4572ffb2cab2c1f74b838
Nigro Simone <nigro.simone@gmail.com>
2023-02-21 21:21:48 +0100

    fix type

--- projects/ng-let/src/lib/ng-let.directive.ts
+++ projects/ng-let/src/lib/ng-let.directive.ts
@@ -74,7 +74,7 @@ export class NgLetDirective<T> {
      * The presence of this method is a signal to the Ivy template type-check compiler that the
      * `NgLet` structural directive renders its template with a specific context type.
      */
-    static ngTemplateContextGuard<T>(dir: NgLetDirective<T>, ctx: any): ctx is NgLetContext<Exclude<T, false | 0 | '' | null | undefined>> {
+    static ngTemplateContextGuard<T>(dir: NgLetDirective<T>, ctx: any): ctx is NgLetContext<T> {
         return true;
     }
 }
