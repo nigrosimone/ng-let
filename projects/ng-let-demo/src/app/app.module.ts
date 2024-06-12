84f4c2fccd47ef6b34867d576529270c6286a662
Nigro Simone <nigro.simone@gmail.com>
2024-06-12 20:47:25 +0200

    Angular 18

--- projects/ng-let-demo/src/app/app.module.ts
+++ projects/ng-let-demo/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { NgLetModule } from '../../../ng-let/src/public-api';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    CommonModule,
-    HttpClientModule,
-    NgLetModule,
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        CommonModule,
+        NgLetModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
